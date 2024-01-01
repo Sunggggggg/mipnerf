f1ad0e08674c29aa30ed7f184cc70c2dd87ed745
sky <sky990319@gmail.com>
2024-01-01 10:53:36 +0900

    f

--- metric.py
+++ metric.py
@@ -4,6 +4,9 @@ from skimage.metrics import structural_similarity, peak_signal_noise_ratio
 from lpips import LPIPS
 
 def get_metric(rgbs, targets, lpips=None, device=torch.device('cuda')):
+    """
+    rgbs, targets : [N, H, W, 3]
+    """
     lpips_vgg = LPIPS(net="vgg").to(device)
 
     rgbs = np.clip(rgbs, 0., 1.).astype(targets.dtype)
@@ -13,8 +16,8 @@ def get_metric(rgbs, targets, lpips=None, device=torch.device('cuda')):
     psnr = np.mean([peak_signal_noise_ratio(targets[i],rgbs[i]) for i in range(test_view)])
     ssim = np.mean([structural_similarity(targets[i], rgbs[i], data_range=rgbs.max()-rgbs.min(), channel_axis=-1) for i in range(test_view)])
 
-    lpips_score = torch.mean(lpips_vgg(torch.from_numpy(targets).permute(2, 0, 1).unsqueeze(0).type(torch.FloatTensor).to(device), 
-                                        torch.from_numpy(rgbs).permute(2, 0, 1).unsqueeze(0).type(torch.FloatTensor).to(device)), dim=0).item()
+    lpips_score = torch.mean(lpips_vgg(torch.from_numpy(targets).permute(0, -1, 1, 2).type(torch.FloatTensor).to(device), 
+                                        torch.from_numpy(rgbs).permute(0, -1, 1, 2).type(torch.FloatTensor).to(device)), dim=0).item()
     
     return psnr, ssim, lpips_score
     
