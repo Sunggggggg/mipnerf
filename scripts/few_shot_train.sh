81cece8c5b5557d5aabaaf339c928cbf12550b16
sky <sky990319@gmail.com>
2024-01-04 14:20:15 +0900

    f

--- scripts/few_shot_train.sh
+++ scripts/few_shot_train.sh
@@ -5,7 +5,7 @@ object="lego hotdog materials chair drums ficus mic ship"
 for obj in $object
 do
     exp=blender_${obj}
-    nerf_config=configs/MipNeRF/${obg}_8shot.txt
+    nerf_config=configs/MipNeRF/${obj}_8shot.txt
     mae_config=configs/MAE/mae.txt
 
     python run_mask_mipnerf.py --expname $exp --nerf_config $nerf_config --mae_config $mae_config --i_print 1
