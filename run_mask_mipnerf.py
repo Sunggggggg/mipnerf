5db45672d2a1bd9e669d48f2a7030c2e83005012
sky <sky990319@gmail.com>
2024-01-05 13:26:49 +0900

    f

--- run_mask_mipnerf.py
+++ run_mask_mipnerf.py
@@ -217,7 +217,7 @@ def train(rank, world_size, args):
                 rgbs_poses = rgbs_poses.type(torch.cuda.FloatTensor).to(rank)        # [1, N, 4, 4]
                 rendered_feat = encoder(rgbs_images, rgbs_poses, mae_input, nerf_input)
                 object_loss_c = mae_loss_func(gt_feat[:, 1:, :], rendered_feat[:, 1:, :])
-                object_loss_c = object_loss_c * args.loss_lam_c * 0.1 / lossmult.to(rank).sum()
+                object_loss_c = object_loss_c * args.loss_lam_c * 0.1 / 64
 
                 # Fine
                 rgbs_images, rgbs_poses = mae_input_format(rgbs_f, sampled_poses, nerf_input, mae_input, args.emb_type)
@@ -225,7 +225,7 @@ def train(rank, world_size, args):
                 rgbs_poses = rgbs_poses.type(torch.cuda.FloatTensor).to(rank)        # [1, N, 4, 4]
                 rendered_feat = encoder(rgbs_images, rgbs_poses, mae_input, nerf_input)
                 object_loss_f = mae_loss_func(gt_feat[:, 1:, :], rendered_feat[:, 1:, :])
-                object_loss_f = object_loss_f * args.loss_lam_f / lossmult.to(rank).sum()
+                object_loss_f = object_loss_f * args.loss_lam_f / 64
 
                 loss += (object_loss_f + object_loss_c)
 
