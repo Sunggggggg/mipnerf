a711246a3065e6cce6b732119c33cccaec529441
sky <sky990319@gmail.com>
2024-01-11 12:45:45 +0900

    f

--- model.py
+++ model.py
@@ -30,10 +30,12 @@ class PositionalEncoding(nn.Module):
             y_enc = torch.cat((y_enc, y_enc), -1)
             x_ret = torch.exp(-0.5 * y_enc) * torch.sin(x_enc)
             y_ret = torch.maximum(torch.zeros_like(y_enc), 0.5 * (1 - torch.exp(-2 * y_enc) * torch.cos(2 * x_enc)) - x_ret ** 2)
+            x_ret = torch.cat([x, x_ret], -1)
             return x_ret, y_ret
         else:
             # PE
             x_ret = torch.sin(x_enc)
+            x_ret = torch.cat([x, x_ret], -1)
             return x_ret
 
 class MipNeRF(nn.Module):
@@ -62,7 +64,7 @@ class MipNeRF(nn.Module):
         self.white_bkgd = white_bkgd
         self.num_levels = num_levels
         self.N_samples = N_samples
-        self.density_input = (max_deg - min_deg) * 3 * 2
+        self.density_input = 3 + (max_deg - min_deg) * 3 * 2
         self.rgb_input = 3 + ((viewdirs_max_deg - viewdirs_min_deg) * 3 * 2)
         self.density_noise = density_noise
         self.rgb_padding = rgb_padding
@@ -170,151 +172,6 @@ class MipNeRF(nn.Module):
             # volumetric rendering
             rgb = raw_rgb * (1 + 2 * self.rgb_padding) - self.rgb_padding
             density = self.density_activation(raw_density + self.density_bias)
-            comp_rgb, distance, acc, weights, alpha = volumetric_rendering(rgb, density, t_vals, rays_d.to(rgb.device), self.white_bkgd)
-            comp_rgbs.append(comp_rgb)
-            distances.append(distance)
-            accs.append(acc)
-        if self.return_raw:
-            raws = torch.cat((torch.clone(rgb).detach(), torch.clone(density).detach()), -1).cpu()
-            # Predicted RGB values for rays, Disparity map (inverse of depth), Accumulated opacity (alpha) along a ray
-            return torch.stack(comp_rgbs), torch.stack(distances), torch.stack(accs), raws
-        else:
-            # Predicted RGB values for rays, Disparity map (inverse of depth), Accumulated opacity (alpha) along a ray
-            return torch.stack(comp_rgbs), torch.stack(distances), torch.stack(accs)
-
-class VanillaMipNeRF(nn.Module):
-    def __init__(self,
-                 use_viewdirs=True,
-                 randomized=False,
-                 ray_shape="cone",
-                 white_bkgd=True,
-                 num_levels=2,
-                 N_samples=64,
-                 N_importance=128,
-                 hidden=256,
-                 density_noise=1, density_bias=-1,
-                 rgb_padding=0.001,
-                 resample_padding=0.01,
-                 min_deg=0, max_deg=16,
-                 viewdirs_min_deg=0, viewdirs_max_deg=4,
-                 device=torch.device("cpu"),
-                 return_raw=False
-                 ):
-        super(MipNeRF, self).__init__()
-        self.use_viewdirs = use_viewdirs
-        self.init_randomized = randomized
-        self.randomized = randomized
-        self.ray_shape = ray_shape
-        self.white_bkgd = white_bkgd
-        self.num_levels = num_levels
-        self.N_samples = N_samples
-        self.N_importance = N_importance
-        self.density_input = (max_deg - min_deg) * 3 * 2
-        self.rgb_input = 3 + ((viewdirs_max_deg - viewdirs_min_deg) * 3 * 2)
-        self.density_noise = density_noise
-        self.rgb_padding = rgb_padding
-        self.resample_padding = resample_padding
-        self.density_bias = density_bias
-        self.hidden = hidden
-        self.device = device
-        self.return_raw = return_raw
-        self.density_activation = nn.Softplus()
-
-        self.positional_encoding = PositionalEncoding(min_deg, max_deg)
-        self.density_net0 = nn.Sequential(
-            nn.Linear(self.density_input, hidden),
-            nn.ReLU(True),
-            nn.Linear(hidden, hidden),
-            nn.ReLU(True),
-            nn.Linear(hidden, hidden),
-            nn.ReLU(True),
-            nn.Linear(hidden, hidden),
-            nn.ReLU(True),
-            nn.Linear(hidden, hidden),
-            nn.ReLU(True)
-        )
-        self.density_net1 = nn.Sequential(
-            nn.Linear(self.density_input + hidden, hidden),
-            nn.ReLU(True),
-            nn.Linear(hidden, hidden),
-            nn.ReLU(True),
-            nn.Linear(hidden, hidden),
-            nn.ReLU(True),
-        )
-        self.final_density = nn.Sequential(
-            nn.Linear(hidden, 1),
-        )
-
-        input_shape = hidden
-        if self.use_viewdirs:
-            input_shape = hidden // 2
-
-            self.rgb_net0 = nn.Sequential(
-                nn.Linear(hidden, hidden)
-            )
-            self.viewdirs_encoding = PositionalEncoding(viewdirs_min_deg, viewdirs_max_deg)
-            self.rgb_net1 = nn.Sequential(
-                nn.Linear(hidden + self.rgb_input, input_shape),
-                nn.ReLU(True),
-            )
-        self.final_rgb = nn.Sequential(
-            nn.Linear(input_shape, 3),
-            nn.Sigmoid()
-        )
-        _xavier_init(self)
-        self.to(device)
-
-    def forward(self, ray_batch):
-        comp_rgbs = []
-        distances = []
-        accs = []
-
-        rays_o, rays_d = ray_batch[:,0:3], ray_batch[:,3:6]                 # [N_rays, 3] each
-        bounds = torch.reshape(ray_batch[...,6:8], [-1,1,2])                # [N_rays, 1, 2]
-        near, far = bounds[...,0], bounds[...,1]                            # [N_rays, 1]
-        radii = torch.reshape(ray_batch[..., 8], [-1, 1])                   # [N_rays, 1]
-        view_dirs = ray_batch[:,-3:] if ray_batch.shape[-1] > 9 else None   # [N_rays, 3]
-        
-        for l in range(self.num_levels):
-            # sample
-            if l == 0:
-                t_vals, (mean, var) = sample_along_rays(rays_o, rays_d, radii, num_samples,
-                                                        near, far, randomized=self.randomized, lindisp=False,
-                                                        ray_shape=self.ray_shape)
-            else:
-                t_vals, (mean, var) = resample_along_rays(rays_o, rays_d, radii, t_vals.to(rays_o.device),
-                                                          weights.to(rays_o.device), 
-                                                          randomized=self.randomized, ray_shape=self.ray_shape)
-            
-            # do integrated positional encoding of samples
-            samples_enc = self.positional_encoding(mean, var)[0]
-            samples_enc = samples_enc.reshape([-1, samples_enc.shape[-1]])  # [N_rays*N_samples, 96]
-
-            # predict density
-            new_encodings = self.density_net0(samples_enc)                  # [N_rays*N_samples, 256]
-            new_encodings = torch.cat((new_encodings, samples_enc), -1)     # [N_rays*N_samples, 256+96]
-            new_encodings = self.density_net1(new_encodings)                # [N_rays*N_samples, 256]
-            raw_density = self.final_density(new_encodings).reshape((-1, self.N_samples, 1)) # [N_rays, N_samples, 1]
-            
-            # predict rgb
-            if self.use_viewdirs:
-                #  do positional encoding of viewdirs
-                viewdirs = self.viewdirs_encoding(view_dirs.to(self.device))             # [N_rays, 27]
-                viewdirs = torch.cat((viewdirs, view_dirs.to(self.device)), -1)          # [N_rays, 30]
-                viewdirs = torch.tile(viewdirs[:, None, :], (1, self.N_samples, 1))         # [N_rays, N_samples, 30]
-                viewdirs = viewdirs.reshape((-1, viewdirs.shape[-1]))                    # [N_rays*N_samples, 30]
-                new_encodings = self.rgb_net0(new_encodings)                             # [N_rays*N_samples, 256]
-                new_encodings = torch.cat((new_encodings, viewdirs), -1)                 # [N_rays*N_samples, 30+256]
-                new_encodings = self.rgb_net1(new_encodings)                             # [N_rays*N_samples, 286]
-            raw_rgb = self.final_rgb(new_encodings).reshape((-1, self.N_samples, 3))   # [N_rays, N_samples, 3]
-            
-            # Add noise to regularize the density predictions if needed.
-            if self.randomized and self.density_noise:
-                raw_density += self.density_noise * torch.rand(raw_density.shape, dtype=raw_density.dtype, device=raw_density.device)
-
-            # volumetric rendering
-            rgb = raw_rgb 
-            density = self.density_activation(raw_density)
             comp_rgb, distance, acc, weights, alpha = volumetric_rendering(rgb, density, t_vals, rays_d.to(rgb.device), self.white_bkgd)
             comp_rgbs.append(comp_rgb)
             distances.append(distance)
